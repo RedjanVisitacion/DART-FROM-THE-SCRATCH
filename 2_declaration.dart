2e7657a48b12c42fe710e5f22904a2ad5e0efc71
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-06-14 15:32:28 +0500

    Declaration

---
+++ 2_declaration.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Numbers: int
+  int age = 19;
+
+  // Numbers: double
+  double grade = 2.25;
+
+  // Strings
+  String subject = "Rizal";
+
+  // Boolean
+  bool status = true;
+
+  // Prints the output
+  print("Subject: " + subject + ":");
+  print("Grade: " + grade.toString());
+
+  print("Single: " + status.toString());
+  print("Age is: " + age.toString());
+}
